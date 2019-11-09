acaa8d87e1adba80a488195f4c9a340438d400b7
IngFerreira <carielfs@gmail.com>
2019-11-09 14:17:21 -0600

    DesarrolladorV2, sigue sin funcionar

--- adminClases/Desarrollador.java
+++ adminClases/Desarrollador.java
@@ -2,41 +2,33 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class Desarrollador extends JFrame{
-
-    public Container cont = getContentPane();
-    public Desarrollador(){
-        //Window name
-        super("nom: Tareas");//nom es el nombre que se manda desde la base de datos
-        getContentPane().setLayout(new FlowLayout(FlowLayout.RIGHT));
-        //Basic elements of the window
-        //Container cont = getContentPane();
-        cont.setLayout(null);
-        JPanel actions = new JPanel();
-        actions.setLayout(null);
-        actions.setBounds(0,0,360,720);
-        actions.setBackground(Color.BLUE);
-        cont.add(actions);
-
+public class Des extends JFrame {
+    public boolean T1=false,T2=false,T3=false,T4=false;//son las tareas que puede tener el desarrolladro
+    public JFrame f=new JFrame();
+    public Des() {
+        f.setLayout(null);
+        setTitle("nom: Tareas");
+        JPanel p=new JPanel();
+        p.setLayout(null);
+        p.setBounds(0,0,360,720);
+        p.setBackground(Color.BLUE);
+        f.add(p);
         setSize(1080,720);
         setVisible(true);
+
     }
-    private void NuevaTarea(){//sigue sin jalar D:
+    public void setT1(){//Hacer un método así por tarea
         JPanel t=new JPanel();
         t.setLayout(null);
         t.setPreferredSize(new Dimension(250,50));
         t.setBackground(Color.RED);
         Button b=new Button("ejemplo");
         t.add(b);
-        cont.add(t);
+        f.add(t);
 
     }
-
-    public static void main(String[] args) {
-        Desarrollador d=new Desarrollador();
-        d.NuevaTarea();
-        //d.NuevaTarea();
-        //d.NuevaTarea();
-        //d.NuevaTarea();
+    public static void main(String[] args){
+        Des des=new Des();
+        des.setT1();
     }
 }
