0ba162cf56a47b45e671d13fe87e0da6c73440b7
IngFerreira <carielfs@gmail.com>
2019-11-15 15:52:06 -0600

    DesarrolladorV5

--- adminClases/Desarrollador.java
+++ adminClases/Desarrollador.java
@@ -4,14 +4,15 @@ import javax.swing.*;
 
 
 public class Des extends JFrame {
-    boolean t1=true,t2=false,t3=false,t4=false;//esto se debería modificar en función de las tareas que tiene asignadas
+    public int contador=0;//cuenta el número de tareas para añadirlas a la pantalla
+    boolean t1=true,t2=true,t3=true,t4=true;//esto se debería modificar en función de las tareas que tiene asignadas, true es que si está asignada, false lo contrario
     public boolean tareas[]={t1,t2,t3,t4};//son las tareas que puede tener el desarrollador
 
     public Des() {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(1080,720);
         setLayout(null);
-        setTitle("nom: Tareas");
+        setTitle("nombre");//Es el nombre del desarrollador
 
         JPanel p=new JPanel();
         p.setLayout(null);
@@ -19,28 +20,24 @@ public class Des extends JFrame {
         p.setBackground(Color.BLUE);
         add(p);
 
-        JPanel infoTarea=new JPanel();
-        infoTarea.setLayout(null);
-        infoTarea.setBounds(460,200,520,350);
-        infoTarea.setBackground(Color.gray);
-        JButton estado=new JButton("Estado");//esto se modificará según quiera el usuario
-        estado.setBounds(920,325,100,50);
-        estado.setBackground(Color.green);
-        infoTarea.add(estado);//No sé porque no aparece el botón
-        add(infoTarea);
-
-        for(Boolean t:tareas){
-            if(t){
-                JButton numTarea=new JButton("tarea n");//n es el número de la tarea asignada
-                numTarea.setBounds(460,150,150,50);//(460+i*150,150,150,50) donde i es el índice de una de las 4 tareas, en este caso empezando con 0
-                add(numTarea);
+        for(Boolean t:tareas){//Crea un panel por tarea
+            if(t){//Aquí se debería poner algo que reciba los datos de la tarea y los agregue al panel
+                JPanel infoTarea=new JPanel();//Crea el panel de la tarea
+                infoTarea.setLayout(new FlowLayout(FlowLayout.RIGHT));
+                infoTarea.setBounds(460,50+contador*150,520,100);
+                infoTarea.setBackground(Color.gray);
+                JButton estado=new JButton("Estado");//esto se modificará según quiera el usuario, falta un método que le de esa acción al botón
+                estado.setPreferredSize(new Dimension(100,50));
+                estado.setBackground(Color.DARK_GRAY);
+                infoTarea.add(estado);
+                add(infoTarea);
+                contador+=1;
             }
         }
-
         setVisible(true);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args){//Esto es solo para probar que si funciona
         Des des=new Des();
     }
 }
